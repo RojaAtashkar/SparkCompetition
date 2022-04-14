27f6a1e5bf7d0c68f9e7900e07ac8c75f0b17a65
RojaAtashkar <103642013+RojaAtashkar@users.noreply.github.com>
2022-04-14 16:35:32 +0430

    Update and rename freeflowSolver to freeFlow.java

--- freeFlow.java
+++ freeFlow.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class freeFlow {
     //public static String[][] graph = new String[20][20];
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
